bacd9f7605e50bc492b7a5ba1c6b0256148ae9cd
kyleochata <kyleochata@gmail.com>
2023-09-22 22:16:01 -0700

    blob and scroll slideshow

---
+++ assets/script/slider.js
@@ -0,0 +1,60 @@
+const track = document.getElementById("image-track");
+
+//mouse click for invisible slider
+const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
+
+//save coordinates and percentages for slider if user undoes click
+const handleOnUp = () => {
+  track.dataset.mouseDownAt = "0";
+  track.dataset.prevPercentage = track.dataset.percentage;
+}
+
+//action of the slider to move through slideshow; also contains the animation for the image themselves to move as well based off of our invis slider
+const handleOnMove = e => {
+  if (track.dataset.mouseDownAt === "0") return;
+
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+    maxDelta = window.innerWidth / 2;
+
+  const percentage = (mouseDelta / maxDelta) * -100,
+    nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
+    nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+
+  track.dataset.percentage = nextPercentage;
+
+  track.animate({
+    transform: `translate(${nextPercentage}%, -50%)`
+  }, { duration: 1200, fill: "forwards" });
+
+  for (const image of track.getElementsByClassName("image")) {
+    image.animate({
+      objectPosition: `${100 + nextPercentage}% center`
+    }, { duration: 1200, fill: "forwards" });
+  }
+}
+
+//mouse blob effect
+const blob = document.getElementById('blob');
+
+//when mouse moves on the body, want the blob to follow
+document.body.onpointermove = event => {
+  const { clientX, clientY } = event;
+
+  blob.animate({
+    left: `${clientX}px`,
+    top: `${clientY}px`
+  }, { duration: 3000, fill: "forwards" }) //how long to take; not to reset prop at end of duration
+}
+
+
+window.onmousedown = e => handleOnDown(e);
+
+window.ontouchstart = e => handleOnDown(e.touches[0]);
+
+window.onmouseup = e => handleOnUp(e);
+
+window.ontouchend = e => handleOnUp(e.touches[0]);
+
+window.onmousemove = e => handleOnMove(e);
+
+window.ontouchmove = e => handleOnMove(e.touches[0]);
